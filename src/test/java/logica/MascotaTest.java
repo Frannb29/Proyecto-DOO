759c99385d75c3f8ca13188b609a263016395d2c
YOUR_NAME <k2117.986@gmail.com>
2026-07-01 21:17:51 -0400

    fix: arregla tests unitarios y coloca la carpeta de los tests en el classpath correcto

---
+++ src/test/java/logica/MascotaTest.java
@@ -0,0 +1,41 @@
+package logica;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MascotaTest {
+
+    @Test
+    public void testCrearMascota() {
+        Mascotas nuevoPerro = MascotaFactory.crearMascota(TipoMascota.PERRO);
+        assertNotNull(nuevoPerro);
+        
+        assertEquals(TipoMascota.PERRO, nuevoPerro.getTipo()); 
+        assertEquals(Habitat.CASA, nuevoPerro.getHabitat());
+    }
+
+    @Test
+    public void testPasarTiempoBajaAlimentacion() {
+        Mascotas otroPerro = new Perro();
+        int alimentacionInicial = otroPerro.getAlimentacion(); 
+        
+        for(int i=0; i<5; i++){
+            otroPerro.pasarTiempo();
+        }
+        assertEquals(alimentacionInicial - 5, otroPerro.getAlimentacion());
+    }
+
+    @Test
+    public void testBajadeAtributosconelTiempo() {
+        Mascotas perro = new Perro(); // Inicialmente todo en 100
+        
+        for(int i = 0; i < 5; i++){
+            perro.pasarTiempo();
+        }
+        
+        assertEquals(95, perro.getAlimentacion());
+        assertEquals(97, perro.getFelicidad());
+        assertEquals(95, perro.getHigiene());
+        assertEquals(98, perro.getSalud());
+    }
+}
